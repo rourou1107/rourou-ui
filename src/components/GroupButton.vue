2bc4347693fd3952a498b5338f528ed23b85ba88
zhangrp <zhangrp@bairuitech.com>
2020-03-28 23:31:53 +0800

    一组按钮完成
    Change-Id: I9c73b67cf562356f1f15c284c21b5177c8d9f588

--- src/components/GroupButton.vue
+++ src/components/GroupButton.vue
@@ -1,9 +1,32 @@
 <template>
-    <div>
+    <div class="g-group-button">
         <slot></slot>
     </div>
 </template>
 <script>
     export default {}
 </script>
-<style></style>
+<style lang="scss">
+    .g-group-button {
+        display: inline-flex;
+        vertical-align: middle;
+        > .g-button {
+            border-radius: 0;
+        }
+        > .g-button:first-child {
+            border-top-left-radius: 4px;
+            border-bottom-left-radius: 4px;
+        }
+        > .g-button:last-child {
+            border-top-right-radius: 4px;
+            border-bottom-right-radius: 4px;
+        }
+        > .g-button:not(first-child) {
+            margin-left: -1px; /*解决2个按钮的左右border连在一起问题*/
+        }
+        > .g-button:hover { /*解决鼠标悬浮时border可能会缺失一边的问题*/
+            position: relative;
+            z-index: 1;
+        }
+    }
+</style>
