709b8d2eeefa820874cf541b3b5cacdf4e3a65dd
zhangrp <zhangrp@bairuitech.com>
2020-03-30 21:47:59 +0800

    消除对 iconfont 的依赖
    Change-Id: I290de8681d647b373614002d6eda64df05a38004

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -4,9 +4,12 @@
     </svg>
 </template>
 <script>
+    // 引入 js 文件就代表运行了这个 js
+    import '../svg';
+
     export default {
         props: ['name']
-    }
+    };
 </script>
 <style lang="scss">
     .icon {
