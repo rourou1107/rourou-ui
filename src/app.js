91807a1cf684bedd581cb5f64d83f30c9b916197
zhangrp <zhangrp@bairuitech.com>
2020-03-31 11:25:39 +0800

    Input 组件样式完成
    Change-Id: Ibcdec103b2ecf6b42acb3a091c04995cc6121b6a

--- src/app.js
+++ src/app.js
@@ -4,12 +4,15 @@ import Icon from "./components/Icon";
 import GroupButton from "./components/GroupButton";
 import chai from 'chai';
 import spies from 'chai-spies';
+import Input from "./components/Input"
 
 chai.use(spies);
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
 Vue.component('g-group-button', GroupButton);
+Vue.component('g-input', Input)
+
 new Vue({
     el: '#app',
     data() {
@@ -18,5 +21,19 @@ new Vue({
             loading2: false,
             loading3: false
         };
+    },
+    methods: {
+        // handleChange() {
+        //     console.log('change')
+        // },
+        // handleInput() {
+        //     console.log('input')
+        // },
+        // handleFocus() {
+        //     console.log('focus')
+        // },
+        // handleBlur() {
+        //     console.log('blur')
+        // }
     }
 });
---
+++ src/components/Input.vue
@@ -0,0 +1,90 @@
+<template>
+    <div class="wrapper">
+        <input class="" :value="value" :disabled="disabled" :readonly="readonly"
+               :class="{error: error}"
+               @change="$emit('change')"
+               @input="$emit('input')"
+               @focus="$emit('focus')"
+               @blur="$emit('blur')"
+        />
+        <template v-if="error">
+            <g-icon name="error" class="input-icon"></g-icon>
+            <span class="input-message">{{error}}</span>
+        </template>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'GInput',
+        props: {
+            value: {
+                type: String
+            },
+            disabled: {
+                type: Boolean,
+                default: false
+            },
+            readonly: {
+                type: Boolean,
+                default: false,
+            },
+            error: {
+                type: String
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+    $height: 32px;
+    $border-color: #999;
+    $border-color-hover: #666;
+    $border-color-disabled: #bbb;
+    $border-radius: 4px;
+    $box-shadow-color: rgba(0, 0, 0, 0.5);
+    $red: #F1453D;
+    $font-size: 14px;
+    .wrapper {
+        font-size: $font-size;
+        display: inline-flex;
+        align-items: center;
+
+        > input {
+            height: $height;
+            padding: 0 8px;
+            border: 1px solid $border-color;
+            border-radius: $border-radius;
+            font-size: inherit;
+
+            &:hover {
+                border: 1px solid $border-color-hover;
+            }
+
+            &:focus {
+                outline: none;
+                box-shadow: inset 0 0 1px 1px $box-shadow-color;
+            }
+
+            &[disabled], &[readonly] {
+                border-color: $border-color-disabled;
+                color: #666;
+                cursor: not-allowed;
+            }
+            &.error {
+                border-color: $red;
+                &:focus {
+                    outline: none;
+                    box-shadow: inset 0 0 1px 1px $red;
+                }
+            }
+        }
+        > .input-icon {
+            fill: $red;
+        }
+        > .input-message {
+            color: $red;
+        }
+        & >:not(last-child) {
+            margin-right: .2em;
+        }
+    }
+</style>
