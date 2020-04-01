ffbed20dbacfb7667aad09cb612094a6865ababa
zhangrp <zhangrp@bairuitech.com>
2020-04-01 22:14:31 +0800

    重构 row 和 col
    Change-Id: I9e397ccf516783ddccc95f00ab3df2263fc3c4e7

--- src/components/Col.vue
+++ src/components/Col.vue
@@ -1,24 +1,35 @@
 <template>
-    <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
-         :style="{paddingLeft: gutter/2+'px', paddingRight: gutter/2+'px'}">
-        <div style="border: 1px solid green;">
-            <slot></slot>
-        </div>
+    <div class="col" :class="colClass"
+         :style="colStyle">
+        <slot></slot>
     </div>
 </template>
 <script>
     export default {
         name: 'GCol',
-        data(){
+        data() {
             return {
-                gutter: ''
+                gutter: 0
             }
         },
         props: {
             span: [Number, String],
             offset: [Number, String],
+        },
+        computed: {
+            colClass() {
+                let {span, offset} = this
+                return [span && `col-${span}`, offset && `offset-${offset}`]
+            },
+            colStyle() {
+                let {gutter} = this
+                return {
+                    paddingLeft: gutter / 2 + 'px',
+                    paddingRight: gutter / 2 + 'px'
+                }
+            }
         }
-    };
+    }
 </script>
 <style lang="scss" scoped>
     .col {
--- src/components/Row.vue
+++ src/components/Row.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="row" :style="{marginLeft: -gutter/2 +'px',marginRight: -gutter/2 +'px'}">
+    <div class="row" :style="rowStyle">
         <slot></slot>
     </div>
 </template>
@@ -14,15 +14,23 @@
         // 先创建爸爸，再创建儿子。儿子挂到爸爸上，爸爸再挂载
         // 所以爸爸挂载后，儿子一定也挂载了
         mounted() {
-            this.$children.forEach((vm)=>{
+            this.$children.forEach((vm) => {
                 vm.$data.gutter = this.gutter
             })
+        },
+        computed: {
+            rowStyle() {
+                let {gutter} = this
+                return {
+                    marginLeft: -gutter / 2 + 'px',
+                    marginRight: -gutter / 2 + 'px'
+                }
+            }
         }
     }
 </script>
 <style lang="scss" scoped>
-    .row{
+    .row {
         display: flex;
-        border: 1px solid red;
     }
 </style>
