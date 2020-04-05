63342b0d5dc7ad4233cd077333869ce8f6a1c016
zhangrp <zhangrp@bairuitech.com>
2020-04-05 13:15:12 +0800

    layout 完成
    Change-Id: Ia7cdfecb51e9df22336ac103ad746e29eb28d326

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ import GroupButton from "./components/GroupButton";
 import Input from "./components/Input"
 import Row from "./components/Row";
 import Col from "./components/Col";
+import Layout from "./components/Layout"
+import Header from "./components/Header"
+import Content from "./components/Content"
+import Footer from "./components/Footer"
+import Sider from "./components/Sider"
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
@@ -12,6 +17,11 @@ Vue.component('g-group-button', GroupButton);
 Vue.component('g-input', Input)
 Vue.component('g-row', Row)
 Vue.component('g-col', Col)
+Vue.component('g-layout', Layout)
+Vue.component('g-header', Header)
+Vue.component('g-content', Content)
+Vue.component('g-footer', Footer)
+Vue.component('g-sider', Sider)
 
 new Vue({
     el: '#app',
---
+++ src/components/Layout.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="layout" :class="layoutClass">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'GLayout',
+        data() {
+            return {
+                layoutClass: {
+                    hasSider: false
+                }
+            }
+        },
+        mounted() {
+            this.$children.forEach(child => {
+                if (child.$options.name === "GSider") {
+                    this.layoutClass.hasSider = true
+                }
+            })
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .layout {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        &.hasSider {
+            flex-grow: 1;
+            flex-direction: row;
+        }
+    }
+</style>
