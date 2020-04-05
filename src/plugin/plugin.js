64e61e758a2c2a5552b2e14a2eb4650a2a96238b
zhangrp <zhangrp@bairuitech.com>
2020-04-05 19:28:57 +0800

    保证页面只有一个 toast
    Change-Id: I75395d3306dc7ed444424ae155d027dd7994cede

--- src/plugin/plugin.js
+++ src/plugin/plugin.js
@@ -1,15 +1,24 @@
 import Toast from "../components/Toast"
 
+let currentToast = null
 export default {
     install(Vue) {
         Vue.prototype.$toast = function(message, toastOptions={}){
-            const Constructor = Vue.extend(Toast)
-            const toast = new Constructor({
-                propsData: toastOptions
-            })
-            toast.$slots.default = [message]
-            toast.$mount()
-            document.body.appendChild(toast.$el)
+            if(currentToast) {
+                currentToast.close()
+            }
+            currentToast = createToast(message, toastOptions, Vue)
         }
     }
+}
+
+function createToast(message, toastOptions, Vue) {
+    const Constructor = Vue.extend(Toast)
+    currentToast = new Constructor({
+        propsData: toastOptions
+    })
+    currentToast.$slots.default = [message]
+    currentToast.$mount()
+    document.body.appendChild(currentToast.$el)
+    return currentToast
 }
